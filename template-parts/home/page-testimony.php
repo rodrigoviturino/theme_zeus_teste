0fe888309788405bb8ff127ae817df62085b1ccd
Rodrigo Viturino <Viturino_souza@outlook.com>
2020-09-08 16:58:21 -0300

    Sessao de testemunhas - finalizado estatico

---
+++ template-parts/home/page-testimony.php
@@ -0,0 +1,64 @@
+<div class="testimony__block-title">
+    <h2 class="title">Check out our customer reviews</h2>
+    <ul class="arrows">
+        <li><i class="fas fa-arrow-left fa-2x"></i></li>
+        <li><i class="fas fa-arrow-right fa-2x"></i></li>
+    </ul>
+</div>
+
+<div class="row">
+
+    <div class="col-md-6">
+        <div class="testimony__card">
+            <div class="testimony__card-thumbnails">
+                <img src="<?= get_template_directory_uri(); ?> . '/assets/images/card-testimony.jpg'">
+            </div>
+            <div class="testimony__card-infos">
+                <h3 class="name">Rodrigo Viturino</h3>
+                <span class="office">Web Development</span>
+                <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer semper metus mi, sit amet cursus ante viverra nec. Vivamus luctus, ipsum eget ornare for a convallis, urna justo rutrum diam, eget tempus lorem.</p>
+                <p class="date">22 May 2020</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-6">
+        <div class="testimony__card">
+            <div class="testimony__card-thumbnails">
+                <img src="<?= get_template_directory_uri(); ?> . '/assets/images/card-testimony.jpg'">
+            </div>
+            <div class="testimony__card-infos">
+                <h3 class="name">Rodrigo Viturino</h3>
+                <span class="office">Web Development</span>
+                <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer semper metus mi, sit amet cursus ante viverra nec. Vivamus luctus, ipsum eget ornare for a convallis, urna justo rutrum diam, eget tempus lorem.</p>
+                <p class="date">22 May 2020</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-6">
+        <div class="testimony__card">
+            <div class="testimony__card-thumbnails">
+                <img src="<?= get_template_directory_uri(); ?> . '/assets/images/card-testimony.jpg'">
+            </div>
+            <div class="testimony__card-infos">
+                <h3 class="name">Rodrigo Viturino</h3>
+                <span class="office">Web Development</span>
+                <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer semper metus mi, sit amet cursus ante viverra nec. Vivamus luctus, ipsum eget ornare for a convallis, urna justo rutrum diam, eget tempus lorem.</p>
+                <p class="date">22 May 2020</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-6">
+        <div class="testimony__card">
+            <div class="testimony__card-thumbnails">
+                <img src="<?= get_template_directory_uri(); ?> . '/assets/images/card-testimony.jpg'">
+            </div>
+            <div class="testimony__card-infos">
+                <h3 class="name">Rodrigo Viturino</h3>
+                <span class="office">Web Development</span>
+                <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer semper metus mi, sit amet cursus ante viverra nec. Vivamus luctus, ipsum eget ornare for a convallis, urna justo rutrum diam, eget tempus lorem.</p>
+                <p class="date">22 May 2020</p>
+            </div>
+        </div>
+    </div>
+
+</div>
