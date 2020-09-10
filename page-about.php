a4e54f14b9454285cd0ca5c9e060aa924a4c9cce
Rodrigo Viturino <Viturino_souza@outlook.com>
2020-09-10 16:01:37 -0300

    Integrando pagina interna about

--- page-about.php
+++ page-about.php
@@ -1,14 +1,43 @@
 <?php
-/* Template Name: Page About */
+/* Template Name: Page About us*/
 get_header();
 ?>
 <?php if(have_posts() ) : while( have_posts() ) : the_post(); ?>
+<main class="pageAbout">
 
-    <section class="intro-interna overlay">
+    <section class="pageAbout__intro-interna overlay">
         <div class="container">
             <?php get_template_part( 'template-parts/about/page', 'teste' ); ?>
         </div>
     </section>
+
+    <section class="pageAbout__about">
+        <div class="container">
+            <?php get_template_part( 'template-parts/home/page', 'about' ); ?>
+
+            <div class="row mt-80">
+               <div class="col-md-6">
+                   <p>Donec mollis purus auctor egestas vestibulum. Ut ut pellentesque quam. Curabitur sed tempor augue. Etiam finibus lectus tempor nisi aliquet, at ultricies felis mollis. Aenean porta metus et sapien posuere, eget efficitur elit dapibus. Duis placerat vitae odio at cursus. Phasellus tempus a dui quis lacinia. Ut eget tellus urna. Pellentesque tincidunt tortor quis leo tincidunt lobortis. Pellentesque ac velit congue, scelerisque velit at, dapibus enim. </p>
+                   <p>Donec mollis purus auctor egestas vestibulum. Ut ut pellentesque quam. Curabitur sed tempor augue. Etiam finibus lectus tempor nisi aliquet, at ultricies felis mollis. Aenean porta metus et sapien posuere, eget efficitur elit dapibus. Duis placerat vitae odio at cursus. Phasellus tempus a dui quis lacinia. Ut eget tellus urna. Pellentesque tincidunt tortor quis leo tincidunt lobortis. Pellentesque ac velit congue, scelerisque velit at, dapibus enim. </p>
+               </div>
+
+               <div class="col-md-6">
+                   <div class="abt-img overlay">
+                        <img src="<?= get_template_directory_uri(); ?> /assets/images/img-about.jpg" class="img-fluid">
+                   </div>
+               </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="differentials overlay">
+        <div class="container">
+            <?php get_template_part( 'template-parts/home/page', 'differentials' ); ?>
+        </div>
+    </section>
+
+
+</main>
 
 <?php endwhile; else: endif; ?>
 
--- template-parts/about/page-teste.php
+++ template-parts/about/page-teste.php
@@ -1,10 +1,22 @@
 <?//php include(TEMPLATEPATH . "/include/intro-interna.php"); ?>
-<div class="intro-interna__wrapper">
-    <?php include(TEMPLATEPATH . "/include/widget-network.php"); ?>
+<div class="pageAbout__intro-interna__wrapper">
+    <div class="pageAbout__intro-interna__wrapper-network">
+        <ul>
+            <li>
+                <a href="#"><i class="fab fa-linkedin"></i></a>
+            </li>
+            <li>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+            </li>
+            <li>
+                <a href="#"><i class="fab fa-facebook-f"></i></a>
+            </li>
+        </ul>
+    </div>
 
-    <div class="intro-interna__wrapper-block-title">
-        <h2>Titulo Página</h2>
-        <p>Lorem ipsum</p>
+    <div class="pageAbout__intro-interna__wrapper-block-title">
+        <p><span class="pageHome">Home</span> <span class="separator">/</span> <span class="pageAtual"><?php the_title(); ?></span></p>
+        <h2 class="title"><?php the_title(); ?></h2>
     </div>
 
 </div>
